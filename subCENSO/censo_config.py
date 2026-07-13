cea7a76316555f2e3cdc73b942e64fb7a1afc9d1
ChristianIeritano <44277344+ChristianIeritano@users.noreply.github.com>
2026-07-13 12:34:19 -0400

    Update for CENSO v3
    - Updated .py submission file for syntax changes with CENSO v3
    - Updated subCENSO for syntax changes with CENSO v3 and regex handling of changes for user-specified charge / multiplicity
    - Uploaded example .cesno2rc in line with syntax in CENSO v3

--- subCENSO/censo_config.py
+++ subCENSO/censo_config.py
@@ -1,45 +1,91 @@
-import os, sys
-from censo.ensembledata import EnsembleData
-from censo.configuration import configure
-from censo.ensembleopt import Prescreening, Screening, Optimization
-#from censo.properties import NMR
-from censo.params import Config
+from censo.ensemble import EnsembleData
+from censo.config.setup import configure
+from censo.ensembleopt import prescreening, screening, optimization
+from censo.properties import nmr
+from censo.config import PartsConfig
+from censo.parallel import get_cluster
+
+from multiprocessing import freeze_support
+import os 
 
 ##########
 #CHECK FOR ACCURATE CHARGE AND MULT INPUTS
 ############
 
-workdir = os.getcwd() # CENSO will put all files in this directory
+def main():
 
-for filename in os.listdir(workdir):
+    workdir = os.getcwd() # CENSO will put all files in this directory
 
-    if filename.lower().endswith('xyz'):
-        input_path = os.path.join(workdir, filename) # path relative to the working directory
+    for filename in os.listdir(workdir):
 
-    if filename.lower().endswith('.censo2rc'):
-        censorc_file = os.path.join(workdir, filename) # path relative to the working directory
+        if filename.lower().endswith('xyz'):
+            ensemble_file = os.path.join(workdir, filename) # path relative to the working directory
 
-ensemble = EnsembleData()
-ensemble.read_input(input_path, charge=0, unpaired=0)
+        if filename.lower().endswith('.censo2rc'):
+            censorc_file = os.path.join(workdir, filename) # path relative to the working directory
 
-#If the user wants to use a specific rcfile:
-configure(censorc_file)
+    ensemble = EnsembleData()
+    ensemble.read_input(ensemble_file, charge=0, unpaired=0)
 
-# Get the number of available cpu cores on this machine
-# This number can also be set to any other integer value and automatically checked for validity
-#ncores = os.cpu_count()
-Config.NCORES = 64
+    # Load a custom rcfile (optional)
+    config = configure(censorc_file)
+
+    # Ensure valid configuration
+    #config.general.solvent = "dmso"
+    config = PartsConfig.model_validate(
+        config.model_dump(),
+        context={
+            "check": [
+                "prescreening",
+                "screening",
+                "optimization",
+                "nmr",
+            ]
+        },
+    )
+
+    # Total cores available to CENSO (SLURM)
+    maxcores = int(
+        os.environ.get(
+            "SLURM_NTASKS",
+            os.cpu_count() or 1,
+        )
+    )
+
+    print(f'maxcores variable: {maxcores}')
+    preferred_ompmin = 4
+    ompmin = max(1, min(preferred_ompmin, maxcores))
+
+    cluster = get_cluster(
+        maxcores=maxcores,
+        ompmin=ompmin,
+    )
+
+    client = cluster.get_client()
+
+    # passing a context enables paths and solvent validation, which is usually skipped
+
+    # Execute workflow steps
+    try:
+        results = [
+            part(ensemble, config, client)
+            for part in [
+        #        prescreening, 
+        #        screening, 
+                optimization, 
+        #        nmr
+            ]
+        ]
+
+    finally:
+        client.close()
+        cluster.close()
+
+    # The results are then also output to json files in the working directory
+    # The molecules stored in the ensemble contain the most up-to-date energy values and geometries
+
+if __name__ == "__main__":
+    freeze_support()
+    main()
 
 
-# Setup and run all the parts that the user wants to run
-# Running the parts in order here, while it is also possible to use a custom order or run some parts multiple times
-# Running a part will return an instance of the respective type
-# References to the resulting part instances will be appended to a list in the EnsembleData object (ensemble.results)
-# Note though, that currently this will lead to results being overwritten in your working directory
-# (you could circumvent this by moving/renaming the folders)
-# Running a part will return it's runtime in seconds
-results, timings = zip(*[part.run(ensemble) for part in [Screening, Optimization]])
-
-# You access the results using the ensemble object
-# You can also find all the results the <part>.json output files
-#print(ensemble.conformers[0].results["prescreening"]["sp"]["energy"])
